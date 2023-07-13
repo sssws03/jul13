d6c2a42819ddc429e3fe9d24d48280e85a11a18e
user <user@DESKTOP-ERR5D88>
2023-07-13 10:32:43 +0900

    first

---
+++ jul13/src/main/java/com/sssws03/web/BoardController.java
@@ -0,0 +1,16 @@
+package com.sssws03.web;
+
+//Ctrl + shift + o --> 임포트 정리해줘요.
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+	//사용자가 /board라고 호출하면 동작할 메소드를 만들겠습니다.
+	
+	@GetMapping("/board")
+	public String board() {
+		return "board"; //jsp파일명
+	}
+}
